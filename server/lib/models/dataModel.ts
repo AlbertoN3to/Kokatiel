0a656751796d32209484390f9f1a2dab9dd43fea
acrn <acrn@cesar.org.br>
2019-04-18 13:13:05 -0300

    Adding routes definition

---
+++ server/lib/models/dataModel.ts
@@ -0,0 +1,60 @@
+import * as mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const DataSchema = new Schema({
+    visitorId: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: String
+    },
+    originUrl: {
+        type: String,
+        required: true
+    },
+    sessionStart: {
+        type: Date,
+        required:true
+    },
+    sessionEnd: {
+        type: Date,
+        required:true
+    },
+    sessionTime: {
+        type: Number,
+        default:0
+    },
+    page: {
+        type: String,
+        required: true
+    },
+    item: {
+        name: String,
+        price: Number,
+        priceUnit: String
+    },
+    geolocalization: {
+        type: String
+    },
+    shoppingCart: [
+        {
+            itemName: String,
+            itemType: String,
+            itemQuantity: Number
+        }
+    ],
+    isVisit: {
+        type:Boolean,
+        required:true 
+    },
+    // userDefined: Schema.Types.Mixed
+    userDefined: [
+        {
+            name:String,
+            value:Number,
+            isBool:Boolean
+        }
+    ]
+});
--- server/lib/routes/routes.ts
+++ server/lib/routes/routes.ts
@@ -1,27 +1,35 @@
-import { VisitsController } from '../controllers/visitsController';
+import { DataController } from '../controllers/dataController';
 import { Request, Response, NextFunction } from 'express';
 var PRIVATE = require('../../config/private.json');
 
 export class Routes {
 
-    public visitsController: VisitsController = new VisitsController();
+    public dataController: DataController = new DataController();
 
     public routes(app): void {
 
-        app.route('/visits/')
-            .post(this.visitsController.addNewVisit)
-            .get((req: Request, res: Response, next:NextFunction) => {
-                if(req.query.key !== PRIVATE.privateKey){
-                    res.status(401).send('Authentication failed.')
-                }else{
-                    next();
-                }
-            },this.visitsController.getVisits);
+        app.route('/data')
+            .post(this.dataController.addNewData)
+        // app.route('/visits/')
+        //     .post(this.visitsController.addNewVisit)
+             .get(this.dataController.getData);
 
-        app.route('/visits/:id')
-            .get(this.visitsController.getVisitById)
-            .put(this.visitsController.updateVisit)
-            .delete(this.visitsController.deleteVisit)
+        app.route('/data/:visitorId')
+             .get(this.dataController.getDataById);
+        //     .put(this.visitsController.updateVisit)
+        //     .delete(this.visitsController.deleteVisit);
         
     }
 }
+
+
+
+
+
+// (req: Request, res: Response, next:NextFunction) => {
+    //         if(req.query.key !== PRIVATE.privateKey){
+    //             res.status(401).send('Authentication failed.')
+    //         }else{
+    //             next();
+    //         }
+        // },
